5f74a5d3ab054dd79994fb84ee3c0ebafe41c388
ericheep <ericheep@gmail.com>
2019-06-03 23:45:47 -0700

    :fire: remove console of keys

--- src/utils/contentful-client.js
+++ src/utils/contentful-client.js
@@ -1,7 +1,5 @@
 import { createClient } from 'contentful';
 
-console.log(process.env.VUE_APP_HEEP_FAMILY_SPACE_ID, process.env.VUE_APP_CONTENTFUL_DELIVERY_TOKEN)
-
 export default createClient({
     space: process.env.VUE_APP_HEEP_FAMILY_SPACE_ID,
     accessToken: process.env.VUE_APP_CONTENTFUL_DELIVERY_TOKEN,
