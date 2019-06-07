e06a21dbc8b67a019d729a51e0556e931107aa7a
ericheep <eric.heep@techmdinc.com>
2019-06-06 17:10:50 -0700

    :boom: add routes, still specifiying site

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import { store } from './store/store'
+import Routes from './routes'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const router = new VueRouter({
+  routes: Routes
+})
 
 new Vue({
   store,
+  router: router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+
+export default [
+  { path: '/', component: 'app' },
+  { path: '/news', component: 'app' }
+]
